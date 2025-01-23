9f4d33e8eac1c85bf6e151c26b41a375a8ac8957
Rajnandini3847 <rajnandinitiwari384@gmail.com>
2025-01-23 13:01:33 +0530

    Basic API call & testing

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -1,20 +1,84 @@
+"use client";
+
 import Link from "next/link";
-export default function RegisterForm (){
-    return (
-        <div className="grid place-content-center h-screen">
-            <div className="shadow-lg p-5 rounded-lg border-t-4 border-green-400">
-                <h1 className="text-lg font-bold my-4">Sign Up</h1>
-                <form className="flex flex-col gap-3">
-                    <input type="text" placeholder="Full name"></input>
-                    <input type="text" placeholder="Email"></input>
-                    <input type="Password" placeholder="Password"></input>
-                    <button className="bg-green-600 cursor-pointer text-white py-2 px-6 font-bold">Register</button>
-                    <div className="bg-red-700 w-fit text-white text-sm py-1 px-3 mt-3 rounded-md">
-                        Error Message
-                    </div>
-                    <Link className="text-right text-sm mt-3" href={'/'}>Already have an account <span className="underline">Login</span></Link>
-                </form>
+import { useState } from "react";
+export default function RegisterForm() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!name || !email || !password ){
+        setError("All fields are required");
+    }
+
+    try {
+        const res= await fetch('api/register',{
+            method: "POST",
+            headers : {
+                "content-Type" : "application/json"
+            },
+            body : JSON.stringify({
+                name, email, password
+            }),
+         })
+
+         if (res.ok) {
+            const form = e.target;
+            form.reset();
+         } else {
+            alert("User registration failed")
+         }
+    } catch (error) {
+        console.log("error occured while registration", error)
+        
+    }
+  }
+
+  console.log("Name :", name);
+  
+
+
+  return (
+    <div className="grid place-content-center h-screen">
+      <div className="shadow-lg p-5 rounded-lg border-t-4 border-green-400">
+        <h1 className="text-lg font-bold my-4">Sign Up</h1>
+
+        <form onSubmit={handleSubmit} className="flex flex-col gap-3 ">
+          <input
+            onChange={(e) => setName(e.target.value)}
+            type="text"
+            placeholder="Full name"
+          />
+          <input
+            onChange={(e) => setEmail(e.target.value)}
+            type="text"
+            placeholder="Email"
+          />
+          <input
+            onChange={(e) => setPassword(e.target.value)}
+            type="Password"
+            placeholder="Password"
+          />
+
+          <button className="bg-green-600 cursor-pointer text-white py-2 px-6 font-bold">
+            Register
+          </button>
+
+          {error && (
+            <div className="bg-red-700 w-fit text-white text-sm py-1 px-3 mt-3 rounded-md">
+              {error}
             </div>
-        </div>
-    )
+          )}
+
+          <Link className="text-right text-sm mt-3" href={"/"}>
+            Already have an account <span className="underline">Login</span>
+          </Link>
+        </form>
+      </div>
+    </div>
+  );
 }
