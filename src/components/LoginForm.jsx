def6b050fd9edb69509d21b0f0058bf45b42f950
Rajnandini3847 <rajnandinitiwari384@gmail.com>
2025-01-23 10:35:45 +0530

    Created Login page

---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,17 @@
+import Link from "next/link"
+export default function LoginForm () {
+    return (
+        <div className="grid place-content-center h-screen">
+            <div className="shadow-lg p-5 rounded-lg border-t-4 border-green-400">
+                <h1 className="text-lg font-bold my-4">Login</h1>
+                <form className="flex flex-col gap-3">
+                    <input type="text" placeholder="Email"/>
+                    <input type="Password" placeholder="Password"/>
+                    <button className="bg-green-500 font-bold text-white cursor-pointer px-6 py-2">Login</button>
+                    <div className="bg-red-600 text-white w-fit text-sm rounded-md px-3 py-1 mt-2 ">Error Message</div>
+                    <Link className="text-sm text-right mt-3 " href={'/register'}>Don't have an Account <span className="underline">Register</span></Link>
+                </form>
+            </div>
+        </div>
+    )
+}
